4d21b699c2df90bc6cee9ae49254d04979645259
nafeyah <nafeyahthenx@gmail.com>
2024-10-19 19:14:58 +0530

    dummy commit

---
+++ src/controllers/openAI/documentEmbeddingController.js
@@ -0,0 +1,5 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+
+const nike10kPdfPath = "backend-neetly/pdf/document.pdf";
+
+exports.generateDocumentEmbeddedText = async (req, res) => {};
