334a2cf87f894472640fb32d657e88c19dc144ca
nafeyah <nafeyahthenx@gmail.com>
2024-10-19 19:21:47 +0530

    test

--- src/controllers/openAI/documentEmbeddingController.js
+++ src/controllers/openAI/documentEmbeddingController.js
@@ -2,4 +2,6 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 const nike10kPdfPath = "backend-neetly/pdf/document.pdf";
 
-exports.generateDocumentEmbeddedText = async (req, res) => {};
+exports.generateDocumentEmbeddedText = async (req, res) => {
+    console.log("hello");
+};
