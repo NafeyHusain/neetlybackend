242bafbd4c9904976dec71b59f41d8b876633586
nafeyah <nafeyahthenx@gmail.com>
2024-10-19 19:32:13 +0530

    document embedding

--- src/controllers/openAI/documentEmbeddingController.js
+++ src/controllers/openAI/documentEmbeddingController.js
@@ -1,5 +1,4 @@
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-
+const PDFLoader = require("@langchain/community/document_loaders/fs/pdf");
 const nike10kPdfPath = "backend-neetly/pdf/document.pdf";
 
 exports.generateDocumentEmbeddedText = async (req, res) => {
